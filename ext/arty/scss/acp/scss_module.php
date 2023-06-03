48b1457fcd4fd2c646f5aad9e2e428d8d9f66de2
coderJeff <coder@execulink.com>
2023-06-03 18:17:23 -0400

    Revert "scss compiler:"
    This reverts commit 423d268c74fd8a34ae43958f5c9b062f0af26e43.

--- ext/arty/scss/acp/scss_module.php
+++ ext/arty/scss/acp/scss_module.php
@@ -10,9 +10,6 @@
 
 namespace arty\scss\acp;
 
-require_once( __DIR__ . '/scssphp/scss.inc.php' );
-use ScssPhp\ScssPhp\Compiler;
-
 class scss_module
 {
 	/** @var string */
@@ -183,7 +180,6 @@ class scss_module
 			'files'	=> array(),
 		);
 
-/*
 		foreach ($scss as $file)
 		{
 			if ($file == 'stylesheet.scss')
@@ -192,14 +188,7 @@ class scss_module
 			}
 			$post['files'][$file] = @file_get_contents($path . $file);
 		}
-*/
-		
-		$compiler = new Compiler();
-		$compiler->setOutputStyle = \ScssPhp\ScssPhp\OutputStyle::EXPANDED;
-		$cssResult = $compiler->compileString( $data, $path . 'stylesheet.scss' )->getCss();
-		$result = json_encode( array('css' => $cssResult ) );
-		
-	/*
+
 		// Send data to server
 		$url = 'http://phpbb31.artodia.com/scss.php';
 
@@ -213,7 +202,6 @@ class scss_module
 			'Content-Length: ' . strlen($encoded))
 		);
 		$result = curl_exec($ch);
-	*/
 
 		if (!strlen($result))
 		{
@@ -335,12 +323,10 @@ class scss_module
 	 */
 	protected function assert_required_modules()
 	{
-/*
 		if (!function_exists('curl_init'))
 		{
 			trigger_error($this->user->lang['ACP_SCSS_MISSING_CURL'] . adm_back_link($this->u_action), E_USER_WARNING);
 		}
-*/
 		if (!function_exists('json_encode'))
 		{
 			trigger_error($this->user->lang['ACP_SCSS_MISSING_JSON'] . adm_back_link($this->u_action), E_USER_WARNING);
